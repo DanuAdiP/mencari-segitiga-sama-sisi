3678b9fbcd52391b3f8dc6b042d5df728cab6a94
DanuAdiP <danu261216@gmail.com>
2019-01-23 22:04:54 +0700

    Commit Pertama

---
+++ 10. sgtgasamasisi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,t,hasil;
+	cout<<"Nama  = Danu Adiputra"<<endl;
+	cout<<"NIM   = 311810929"<<endl;
+	cout<<"Kelas = 18.B.2"<<endl<<endl;
+	cout<<"RUMUS MENGHITUNG LUAS SEGITIGA SAMA SISI"<<endl<<endl;
+	cout<<"Masukkan alas   = ";cin>>a;
+	cout<<"Masukkan tinggi = ";cin>>t;
+	hasil=0.5*a*t;
+	cout<<"Luasnya adalah  = "<<hasil;
+}
